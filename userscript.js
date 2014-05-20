5979381e8400655897f6af45bbf8dc6803b929f8
thevdude <rb.cubed@gmail.com>
2014-05-20 02:21:42 -0400

    Added matchID that will eventually have team names and colors, quick example of looking through players to get names/teams

--- userscript.js
+++ userscript.js
@@ -1,6 +1,9 @@
 var scorebot = io.connect('http://home.shadomoonaussies.com:3030');
+var matchID = {"server": tagpro.host.split('.')[0].split('-')[1], "port": tagpro.host.split(':')[1], "time": tagpro.gameEndsAt};
 
-//for now, we'll just put the most recent in a top left scoreboard
+// for now, we'll just put the most recent in a top left scoreboard
+// I'm going to talk to omicron about using prettyText to match all of the rest
+// of the text on the page, possibly replacing the FPS/PING/LOSS
 var scoreboard = document.createElement('div');
 scoreboard.style.position = "absolute";
 scoreboard.style.top = "0px";
@@ -11,15 +14,17 @@ scorebot.on('sbScoreUpdate', function (e) {
   scoreboard.innerHTML = e.score.r + " - " + e.score.b;
 }
 
-// this will make a call to the server to get the scores
-scorebot.emit('sbGetScores');
-
 // this will send the server a message with the score every time a cap happens, including a matchID
 tagpro.socket.on('score', function (e) { scorebot.emit('message', {"match": matchID, "score": { "r": e.r, "b": e.b }); });
 
+// TODO: Loop through players and determine teams from that, this will let us
+// set up the ticker like "BNB 2 - 1 TA" and have them properly colored, as well.
+//
+// We only have to check for one player from each team matching, as well. Once
+// we have one from each team, we'll know colors and team names.
 
-// Can use groupID from within game? 
-// YES, tagpro.group.socket.name
+// for (x in tagpro.players) {
+//   console.log(tagpro.players[x].name);
+// }
 
-// Find good way to make uniqID for matches, and auto group matches from the same client possibly?
-// Still not a great way to do this, can use time, server, port, and map to get close.
+// we'll want a roster object/array of some sort, I'll think over this at work
