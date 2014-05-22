0c578255571a22bdbd8f76a9b4ac35a4068040f2
thevdude <rb.cubed@gmail.com>
2014-05-22 19:01:15 -0400

    will check and set teams, and send them along with score updates

--- sockettest.js
+++ sockettest.js
@@ -1,11 +1,14 @@
 // Socket.io that listens for score updates, and sends them to clients
 // Author: thevdude (rb.cubed@gmail.com)
 //
-// Node Modules: socket.io
+// Node Modules: socket.io, mongodb
 
 var app = require('http').createServer(handler),
-  io = require('socket.io').listen(app),
-  fs = require('fs')
+	io = require('socket.io').listen(app),
+	fs = require('fs'),
+  mdb = require('mongodb').MongoClient,
+	redTeam = "",
+  blueTeam = ""
 
 
 app.listen(3030);
@@ -14,14 +17,35 @@ app.listen(3030);
 function handler (req, res) {
 }
 
+function getTeamAbbr (user) {
+	mdb.connect('mongodb://localhost/tagproteams', function (err, db){
+		if (err) { return console.log(err); }
+		var teams = db.collection('teams');
+		teams.findOne({players: user.name}, function (e, d){
+			if ( user.team === 1 ) {
+				redTeam = d.abbr;
+			} else if ( user.team === 2 ) {
+				blueTeam = d.abbr;
+			}
+		})
+	})
+}
+
 io.sockets.on('connection', function (socket) {
-  // All we really need is for this to pass the score along, so that's all it's doing.
-  socket.on('message', function (data) {
-  	data.redTeam = "red";
-  	data.blueTeam = "blue"; //<--- obviously will take care of that.
-    console.log(data);
-    socket.broadcast.emit('sbScoreUpdate', data);
-  });
-  socket.on('disconnect', function() {
-  });
+	// All we really need is for this to pass the score along, so that's all it's doing.
+	socket.on('message', function (data) {
+		data.redTeam = redTeam;
+		data.blueTeam = blueTeam; //<--- obviously will take care of that.
+		console.log(data);
+		socket.broadcast.emit('sbScoreUpdate', data);
+	});
+	socket.on('players', function(data){
+		for (x in data.players) {
+			if (redTeam === "" || blueTeam === "") {
+				getTeamAbbr(data.players[x]);
+			}
+    }
+	});
+	socket.on('disconnect', function() {
+	});
 });
